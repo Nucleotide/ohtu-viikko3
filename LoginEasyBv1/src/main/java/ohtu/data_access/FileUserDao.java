697021739b9adaa97701fd81d7407c18f15a1067
Jone <joonas.laakkonen@gmail.com>
2014-03-27 17:14:12 +0200

    Task 7 done

--- LoginEasyBv1/src/main/java/ohtu/data_access/FileUserDao.java
+++ LoginEasyBv1/src/main/java/ohtu/data_access/FileUserDao.java
@@ -1,44 +1,81 @@
-//package ohtu.data_access;
-//
-//import java.io.File;
-//import java.io.FileNotFoundException;
-//import java.io.FileWriter;
-//import java.io.IOException;
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Scanner;
-//import ohtu.domain.User;
-//
-///**
-// *
-// * @author joonaslaakkonen
-// */
-//public class FileUserDao implements UserDao{
-//    
-//    private List<User> users;
-//    private Scanner tiedosto;
-//    private FileWriter kirjoittaja;
-//
-//    public FileUserDao(File lista) throws FileNotFoundException, IOException {
-//        this.tiedosto = new Scanner(lista);
-//        this.kirjoittaja = new FileWriter(lista);
-////        users = new ArrayList<User>();
-////        users.add(new User("pekka", "akkep"));
-//    } 
-//
-//    @Override
-//    public List<User> listAll() {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//
-//    @Override
-//    public User findByName(String name) {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//
-//    @Override
-//    public void add(User user) {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-//    }
-//    
-//}
+package ohtu.data_access;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import ohtu.domain.User;
+
+/**
+ *
+ * @author joonaslaakkonen
+ */
+public class FileUserDao implements UserDao{
+
+    private Scanner tiedosto;
+    private FileWriter kirjoittaja;
+    private ArrayList<User> lista;
+    private File uusi;
+    private String polku;
+    
+    public FileUserDao(String polku) throws FileNotFoundException, IOException {
+        this.uusi = new File(polku);
+        this.polku = polku;
+        this.tiedosto = new Scanner(uusi);
+        
+        this.lista = new ArrayList<User>();
+        while (this.tiedosto.hasNext()) {
+            String[] rivi = this.tiedosto.nextLine().split(";");
+            String nimi = rivi[0];
+            String salasana = rivi[1];
+            this.lista.add(new User(nimi, salasana));
+        }
+    } 
+
+    @Override
+    public List<User> listAll() {
+        return lista;
+    }
+
+    @Override
+    public User findByName(String name) {
+        for (User apu : this.lista) {
+            if (apu.getUsername().equals(name)) {
+                return apu;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void add(User user) {
+        try {
+            this.kirjoittaja = new FileWriter(new File(this.polku));
+        } catch (IOException ex) {
+            System.out.println("Vituiks meni");
+            Logger.getLogger(FileUserDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        String uusiLista = "";
+        this.lista.add(user);
+        try {
+            for (User apu : this.lista){
+                uusiLista += apu.getUsername() + ";" + apu.getPassword() + "\n";
+            }
+            this.kirjoittaja.append(uusiLista);
+        } catch (IOException ex) {
+            System.out.println("Kirjoittaminen meni vituiks");
+            Logger.getLogger(FileUserDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            this.kirjoittaja.close();
+        } catch (IOException ex) {
+            Logger.getLogger(FileUserDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
